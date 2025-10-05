e6ab777b62fa8c0cbe9a7674f041229de36aa652
Szerwigi1410 <szerwigi14@gmail.com>
2025-10-05 14:33:16 +0200

    1

--- SHerFile.sh
+++ SHerFile.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Hey it's Szerwigi1410
+# For now ts only works with kitty
+
+# files in the current folders
+files=(*)
+
 #----COLOR-CODES:
 
 GREEN="\033[32m"
@@ -14,30 +20,76 @@ RESET="\033[0m"
 BLACK="\033[30m"
 GRAY="\033[90m"
 
+#--------CONFIG
+CONFIG_FILE="$HOME/.config/SHerFile/config"
+
+# If there is no config – create a default one.
+if [[ ! -f "$CONFIG_FILE" ]]; then
+    mkdir -p "$(dirname "$CONFIG_FILE")"
+    echo -e "# Your IDE or text editor:" > "$CONFIG_FILE"
+    echo -e "EDITOR=nano\n" >> "$CONFIG_FILE"
+
+fi
+
+# Load values from the config
+source "$CONFIG_FILE"
+
+IDE="$EDITOR"
+
+#--------output .txt file
 OUTPUT="temp.txt"
 >$OUTPUT;
 
+#--------Main window
 dialog --clear --title "SHerFile" --backtitle "Szerwigi's Bash File manager" --menu "File actions" 15 40 5 \
-        1 "List files" \
-        2 "List all files" \
-        3 "Custom command" \
-        4 "Custom command --hold" \
-        5 "Exit" 2>menu_choice.txt
+        1 "List files and folders" \
+        2 "List files UI" \
+        3 "List all files and folders" \
+        4 "Custom command" \
+        5 "Custom command --hold" \
+        6 "Exit" 2>menu_choice.txt
 
 choice=$(<menu_choice.txt)
 clear
 
+#-------cases for main window
 case $choice in
-    1) ls --color=auto;;
-    2) ls --color=auto -a;;
-    3) dialog --clear --title "Inputbox" --backtitle "Szerwigi's Bash File manager" --inputbox "Custom command:" 10 40 "" 2> $OUTPUT
+    1) ls --color=auto
+        ;;
+    2)  if [ ${#files[@]} -eq 0 ]; then
+            dialog --msgbox "No files!" 6 40
+            clear
+            exit 1
+        fi
+
+        menu_items=()
+        for file in "${files[@]}"; do
+            menu_items+=("$file" "")
+        done
+
+        choice1=$(dialog --clear --backtitle "Szerwigi's Bash File manager" --menu "Choose the file to open (nano):" 15 50 10 "${menu_items[@]}" 3>&1 1>&2 2>&3)
+
+        exit_status=$?
+        clear
+
+        if [ $exit_status -eq 0 ] && [ -n "$choice1" ]; then
+            $IDE "$choice1"
+        else
+            echo "Exitting..."
+        fi
+        ;;
+    3) ls --color=auto -a
+        ;;
+    4) dialog --clear --title "Inputbox" --backtitle "Szerwigi's Bash File manager" --inputbox "Custom command:" 10 40 "" 2> $OUTPUT
        kitty $(<$OUTPUT)
        clear
        ;;
-    4) dialog --clear --title "Inputbox" --backtitle "Szerwigi's Bash File manager" --inputbox "Custom command --hold:" 10 40 "" 2> $OUTPUT
+    5) dialog --clear --title "Inputbox" --backtitle "Szerwigi's Bash File manager" --inputbox "Custom command --hold:" 10 40 "" 2> $OUTPUT
        kitty --hold $(<$OUTPUT)
        clear
        ;;   
-    5) echo "Exiting...";;
-    *) echo "Invalid choice";;
+    6) echo "Exiting..."
+       ;;
+    *) echo "Invalid choice"
+       ;;
 esac
