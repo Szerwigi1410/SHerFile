fa79caab8e24d4956dbacacc5188351a4947b48a
Szerwigi1410 <szerwigi14@gmail.com>
2025-10-05 14:11:43 +0200

    kinda adding stuff

--- SHerFile.sh
+++ SHerFile.sh
@@ -14,12 +14,15 @@ RESET="\033[0m"
 BLACK="\033[30m"
 GRAY="\033[90m"
 
-dialog --clear --menu "File actions" 15 30 5 \
+OUTPUT="temp.txt"
+>$OUTPUT;
+
+dialog --clear --title "SHerFile" --backtitle "Szerwigi's Bash File manager" --menu "File actions" 15 40 5 \
         1 "List files" \
         2 "List all files" \
-        3 "Exit" 2>menu_choice.txt
-
-#configs=("Hyprland" "Waybar" "Quit")
+        3 "Custom command" \
+        4 "Custom command --hold" \
+        5 "Exit" 2>menu_choice.txt
 
 choice=$(<menu_choice.txt)
 clear
@@ -27,6 +30,14 @@ clear
 case $choice in
     1) ls --color=auto;;
     2) ls --color=auto -a;;
-    3) echo "Exiting...";;
+    3) dialog --clear --title "Inputbox" --backtitle "Szerwigi's Bash File manager" --inputbox "Custom command:" 10 40 "" 2> $OUTPUT
+       kitty $(<$OUTPUT)
+       clear
+       ;;
+    4) dialog --clear --title "Inputbox" --backtitle "Szerwigi's Bash File manager" --inputbox "Custom command --hold:" 10 40 "" 2> $OUTPUT
+       kitty --hold $(<$OUTPUT)
+       clear
+       ;;   
+    5) echo "Exiting...";;
     *) echo "Invalid choice";;
 esac
